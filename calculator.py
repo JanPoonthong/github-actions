68de00b49b96cb2532fa25c5d5ffe3244c90d64a
Jan Poonthong <janpoonthong628@gmail.com>
2025-01-03 09:08:46 +0700

    JIRA-1: Implement multiply function

--- calculator.py
+++ calculator.py
@@ -6,6 +6,11 @@ def subtract(a, b):
     """Subtract two numbers."""
     return a - b
 
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
+    print("Multiply:", multiply(5, 3))
