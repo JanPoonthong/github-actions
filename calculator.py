c4574c441e136ed281d24f3a62d3bf5adeeca0f9
Jan Poonthong <janpoonthong628@gmail.com>
2025-01-03 08:54:30 +0700

    feat: basic calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition:", add(5, 3))
+    print("Subtraction:", subtract(5, 3))
